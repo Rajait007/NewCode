11f5b3cd3b7fb50670271da0ca34c6190d0c2d1e
Rajait007 <Raj.ait007@gmail.com>
2020-08-21 15:55:11 +0530

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+var server = app.listen(3000, () => {
+    console.log("Listening on port " + server.address().port + "...");
+});
+
+module.exports = server;
