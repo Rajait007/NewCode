f3f13d95b66907f9dad9f8230a4794025f09467d
Rajait007 <Raj.ait007@gmail.com>
2020-09-11 13:46:26 +0530

    changes

--- spec/helloWorld-spec.js
+++ spec/helloWorld-spec.js
@@ -1,22 +1,22 @@
-import { get } from "request";
+var request = require("request");
 var base_url = "http://localhost:3000/";
-import { close } from "../app.js";
+var server = require("../app.js");
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 500", function(done) {
-      get(base_url, function(error, response, body) {
+      request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(500);
         done();
       });
     });
 
     it("returns Hello World", function(done) {
-      get(base_url, function(error, response, body) {
+      request.get(base_url, function(error, response, body) {
         expect(body).toBe("Hello World");
         done();
 
-        close();
+        server.close();
         //10th  sep 2nd fail case
         //
       });
