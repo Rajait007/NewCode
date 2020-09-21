361e86d8e0ae65810a74886c8fa874a1f3720e98
Rajait007 <Raj.ait007@gmail.com>
2020-09-21 14:30:45 +0530

    test5

--- spec/helloWorld-spec.js
+++ spec/helloWorld-spec.js
@@ -1,13 +1,13 @@
-import { get } from "request";
-import { get as _get } from 'frisby';
+var request = require("request");
+const frisby = require('frisby');
 var base_url = "http://localhost:3000/";
 
-import { close } from "../app.js";
+var server = require("../app.js");
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
-      get(base_url, function(error, response, body) {
+      request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
@@ -17,7 +17,8 @@ describe("Hello World Server", function() {
     describe("GET City/delhi/1", function() {
  
       it("should return the City name in caps and a city code", function(done) {
-      _get(base_url+"City/delhi/1")
+      frisby
+        .get(base_url+"City/delhi/1")
         .then(function(response) {
           expect(response.json.City.Name).toBe("DELHI");
           expect(response.json.City.Code).toBe("1");
@@ -29,7 +30,8 @@ describe("Hello World Server", function() {
   describe("GET Hospital/:Apolo", function() {
  
     it("should return the hospital  name in caps and a type of hospital", function(done) {
-    _get(base_url+"Hospital/Apolo")
+    frisby
+      .get(base_url+"Hospital/Apolo")
       .then(function(response) {
         expect(response.json.Hospital.Name).toBe("APOLO");
         expect(response.json.Hospital.type).toBe("dental");
@@ -40,7 +42,7 @@ describe("Hello World Server", function() {
 }); 
 
     it("returns only Delhi", function(done) {
-      get(base_url+"1", function(error, response, body) {
+      request.get(base_url+"1", function(error, response, body) {
         expect(body).toBe("Delhi");
         done();
       });
@@ -48,11 +50,11 @@ describe("Hello World Server", function() {
 
 
     it("returns Hello World", function(done) {
-      get(base_url, function(error, response, body) {
+      request.get(base_url, function(error, response, body) {
         expect(body).toBe("Hello World!");
         done();
 
-        close();
+        server.close();
         //10th  sep fail case
       });
     });
